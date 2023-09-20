19651210e9166e8e040bb312f6cab68da3d56686
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-20 18:41:00 +0000

    conversion

---
+++ 03_conversion.js
@@ -0,0 +1,61 @@
+//let score = "undefined";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+
+//let isLoggedIn = 1
+
+
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// *********************** operation  **********************************   ctrl + / for line comment
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/3);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " ajay"
+
+let str3 = str1+str2
+
+console.log(str3);
+
+
+console.log(+true)
+
+
+
+
+
+
+
+
+
+
+
+
+
