821979a9c65bf08947daed4bea308b2462a64212
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-20 18:36:31 +0000

    how to use variable ?

---
+++ 01_var.js
@@ -0,0 +1,17 @@
+const accountId = 144353
+let email = "test@gmailcom"
+var accountPass = "12345"
+accountCity = "delhi"
+let accountState;
+
+//accountId = 2
+
+email = "testi@gmail.com"
+accountPass = "22365"
+accountCity = "gurgaon"
+
+console.log(accountId);
+console.log(email);
+console.log(accountPass);
+console.log(accountCity);
+console.table([accountId,accountPass,email,accountCity,accountState])
