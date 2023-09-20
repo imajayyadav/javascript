becf1e9d48c7d67ac158738afea5fac5e3e087ec
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-20 18:43:41 +0000

    string interpolation

---
+++ 05_string.js
@@ -0,0 +1,18 @@
+const name = "ajay"
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+
+const gameName = new String('ajay');
+
+
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+
+console.log(gameName.substring(0,2))
+
