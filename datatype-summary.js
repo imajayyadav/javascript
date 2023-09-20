ca13c9be77e22b9ca3fa503f8e0d307b77aff053
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-20 18:46:57 +0000

    datatype summary

---
+++ datatype-summary.js
@@ -0,0 +1,40 @@
+
+// Primitive dataype
+
+// 7 types =>  String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+
+// let userEmail;
+
+// const id = Symbol('123')
+// const bigNumber = 54654684645454565543535148
+
+
+
+// Non - Primitive => reference type
+
+//array , objects, functions
+
+
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+
+// let myObj = {
+//     name: "ajay",
+//     age: 22
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world")
+// }
+
+// console.log(typeof heros)
+
+
+
