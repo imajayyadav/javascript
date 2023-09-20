9c3d15b75ea472d0e06a9bab99ef02be9e2e6f8b
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-20 18:45:02 +0000

    number and math uses

---
+++ 06_numsandmath.js
@@ -0,0 +1,31 @@
+// const  score = 10400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(typeof balance);
+
+
+
+// const otherNumber = 123.8956;
+// console.log(otherNumber.toPrecision(3));
+
+/////////// Maths-------------------
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.min(8,45,89,54,56));
+// console.log(Math.max(8,45,89,54,56));
+
+
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
