e98b68d30c4db5bc69918caecdfd519ec694182b
Ajay yadav <103932221+imajayyadav@users.noreply.github.com>
2023-09-24 15:55:29 +0000

    how to use date

---
+++ 07_date.js
@@ -0,0 +1,11 @@
+//Dates 
+
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+
+
